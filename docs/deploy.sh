1e27276b6566ad04e42baaeb021f39da682b5b48
akisute514 <akisute514@gmail.com>
2020-07-12 02:58:45 +0900

    setup circle-ci

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-remote_url = $(git config --get remote.origin.url)
+remote_url=$(git config --get remote.origin.url)
 cd build/html
 git init
 git config --global user.name "circle-ci"
@@ -7,5 +7,4 @@ git config --global user.email "<>"
 git add -A
 git commit -m "[ci skip] deploy documents by circle-ci"
 echo ${remote_url}
-git remote add origin ${remote_url}
-git push -f origin master:gh-pages
+git push -f ${remote_url} master:gh-pages
