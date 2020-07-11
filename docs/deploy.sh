fc7844df056e8710af49415281aeaadd926449fa
akisute514 <akisute514@gmail.com>
2020-07-12 04:09:24 +0900

    fix css path on gh-pages

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -2,6 +2,7 @@
 remote_url=$(git config --get remote.origin.url)
 cd build/html
 git init
+touch .nojekyll
 git config --global user.name "circle-ci"
 git config --global user.email "<>"
 git add -A
