bf7c7cad2b275b1d6a96452ec768e1f6a1dd3eab
akisute514 <akisute514@gmail.com>
2020-07-12 02:30:05 +0900

    setup circle-ci

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -5,4 +5,5 @@ git init
 git config --global user.name "circle-ci"
 git config --global user.email "<>"
 git add -A
+git commit -m "[ci skip] deploy documents by circle-ci"
 git push -f $(remote_url) master:gh-pages
